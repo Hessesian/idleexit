0a27c0785cbc1f9b35226c6f123a13e65a625b8d
GitHub Action <action@github.com>
2023-06-15 18:44:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/hessesian/idleexit/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hessesian/idleexit/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.hessesian.idleexit.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hessesian.idleexit.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
